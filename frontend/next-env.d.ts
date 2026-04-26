e8a666242ec539894b1eb057c90e5e5f2389dd92
axmz-dev <alexander.marquez@gmail.com>
2026-04-26 02:11:49 -0500

    despliegue qa dom 26/04/2026  2:11:49.16

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/dev/types/routes.d.ts";
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
->>>>>>> 0c35a1da603056d1a9902e83e791ca27a82e242c
